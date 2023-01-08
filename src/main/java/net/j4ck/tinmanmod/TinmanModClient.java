ce820f641ba5e05c31ebd361d3f1563ed7c77757
J4ckTheSn4ck <jacksonbenoitlong@gmail.com>
2023-01-08 01:13:56 -0600

    Added important comment

---
+++ src/main/java/net/j4ck/tinmanmod/TinmanModClient.java
@@ -0,0 +1,10 @@
+package net.j4ck.tinmanmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TinmanModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
